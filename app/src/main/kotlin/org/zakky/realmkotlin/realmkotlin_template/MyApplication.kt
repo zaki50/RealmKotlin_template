52fa8dcc87201b164b1d89b62f0cc1ebd0c1c0d6
Makoto Yamazaki <makoto1975@gmail.com>
2017-03-04 16:00:33 +0900

    component can be a field of MyApplication

--- app/src/main/kotlin/org/zakky/realmkotlin/realmkotlin_template/MyApplication.kt
+++ app/src/main/kotlin/org/zakky/realmkotlin/realmkotlin_template/MyApplication.kt
@@ -5,11 +5,9 @@ import io.realm.Realm
 
 class MyApplication : Application() {
 
-    companion object {
-        val component: MainComponent = DaggerMainComponent.builder()
-                .realmConfigComponent(DaggerRealmConfigComponent.create())
-                .build()
-    }
+    val component: MainComponent = DaggerMainComponent.builder()
+            .realmConfigComponent(DaggerRealmConfigComponent.create())
+            .build()
 
     override fun onCreate() {
         super.onCreate()
